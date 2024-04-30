6d7d7fc5ac05a1ae307e84dbcc673338686c358a
characterMi <abol1385fx@gmail.com>
2024-04-30 20:10:13 +0330

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -18,15 +18,11 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
-      const fetchPromise = fetch(e.request).then((networkResponse) => {
-        caches.open("assets").then((cache) => {
-          cache.put(e.request, networkResponse.clone());
-
-          return networkResponse;
-        });
-      });
-
-      return response || fetchPromise;
+      if (response) {
+        return response;
+      } else {
+        return fetch(e.request);
+      }
     })
   );
 });
