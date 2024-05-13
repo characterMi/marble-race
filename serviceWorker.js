eeb81cc956444a5b27681bf3f6f41a5aa0799f7e
characterMi <abol1385fx@gmail.com>
2024-05-13 17:53:32 +0330

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,9 +4,9 @@ const assets = [
   "/marble-race/trophy/scene.bin",
   "/marble-race/trophy/textures/Object001_mtl_baseColor.jpeg",
   "/marble-race/fonts/Nunito-ExtraLight.ttf",
-  "/marble-race/assets/index-0ad0f83c.js",
+  "/marble-race/assets/index-8b678066.js",
   "/marble-race/assets/index-71fa28c1.css",
-  "/marble-race/assets/troika-three-text.esm-134c2196.js",
+  "/marble-race/assets/troika-three-text.esm-ed55d9e9.js",
 ];
 
 self.addEventListener("install", (e) => {
@@ -16,18 +16,6 @@ self.addEventListener("install", (e) => {
     })
   );
 });
-
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     caches.match(e.request).then((response) => {
-//       if (response) {
-//         return response;
-//       } else {
-//         return fetch(e.request);
-//       }
-//     })
-//   );
-// });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
