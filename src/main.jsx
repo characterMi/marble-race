52a4e9eaf1cecab87b1f4639dff469d823b3aa4e
characterMi <abol1385fx@gmail.com>
2024-05-20 18:39:18 +0330

    add an error event for the window

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import Scene from "./Scene";
 
-import './style.css'
+import './style.css';
 
 createRoot(document.getElementById("root")).render(<Scene />);
