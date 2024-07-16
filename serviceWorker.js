b0e1e2e253292cdef67167f64d41f935611ca173
characterMi <abol1385fx@gmail.com>
2024-07-16 23:10:57 +0330

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,9 +4,9 @@ const assets = [
   "/marble-race/trophy/scene.bin",
   "/marble-race/trophy/textures/Object001_mtl_baseColor.jpeg",
   "/marble-race/fonts/Nunito-ExtraLight.ttf",
-  "/marble-race/assets/index-4e9a5faa.js",
-  "/marble-race/assets/index-65e99ed3.css",
-  "/marble-race/assets/troika-three-text.esm-3bb7cd89.js",
+  "/marble-race/assets/index-7f579878.js",
+  "/marble-race/assets/index-ca01007b.css",
+  "troika-three-text.esm-4a861bd8.js",
 ];
 
 self.addEventListener("install", (e) => {
@@ -17,18 +17,31 @@ self.addEventListener("install", (e) => {
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      const fetchPromise = fetch(e.request).then((networkResponse) => {
-        caches.open("assets").then((cache) => {
-          cache.put(e.request, networkResponse.clone());
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Even if the response is in the cache, we fetch it
+      // and update the cache for future usage
+      const fetchPromise = fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open("assets").then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch((e) => {
+          console.error(e);
 
-          return networkResponse;
+          return new Response(
+            "Network error and no cached data available. see the browser's console for more information",
+            {
+              status: 503,
+              statusText: "Service Unavailable.",
+            }
+          );
         });
-      });
-
-      return response || fetchPromise;
+      // We use the currently cached version if it's there
+      return response || fetchPromise; // cached or a network fetch
     })
   );
 });
