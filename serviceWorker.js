65ca916c9070540d88da02104be072838e58144f
characterMi <abol1385fx@gmail.com>
2024-07-17 17:16:37 +0330

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -11,7 +11,7 @@ const assets = [
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("assets").then((cache) => {
+    caches.open("marble-race-game").then((cache) => {
       cache.addAll(assets);
     })
   );
@@ -19,29 +19,35 @@ self.addEventListener("install", (e) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Even if the response is in the cache, we fetch it
-      // and update the cache for future usage
-      const fetchPromise = fetch(event.request)
-        .then((networkResponse) => {
-          return caches.open("assets").then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        })
-        .catch((e) => {
-          console.error(e);
+    caches
+      .match(event.request) // searching in the cache
+      .then((response) => {
+        if (response) {
+          // The request is in the cache
+          return response; // cache hit
+        } else {
+          // We need to go to the network
+          const fetchPromise = fetch(event.request)
+            .then((networkResponse) => {
+              return caches.open("marble-race-game").then((cache) => {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+              });
+            })
+            .catch((e) => {
+              console.error(e);
 
-          return new Response(
-            "Network error and no cached data available. see the browser's console for more information",
-            {
-              status: 503,
-              statusText: "Service Unavailable.",
-            }
-          );
-        });
-      // We use the currently cached version if it's there
-      return response || fetchPromise; // cached or a network fetch
-    })
+              return new Response(
+                "Network error and no cached data available. see the browser's console for more information",
+                {
+                  status: 503,
+                  statusText: "Service Unavailable.",
+                }
+              );
+            });
+
+          return fetchPromise; // cache miss
+        }
+      })
   );
 });
